bae2c5af81cc0f69aa0f26ebc749d353f754547d
Vishal <112686929+VishalGaud17@users.noreply.github.com>
2025-09-04 17:08:53 +0530

    Create 05_cleanup.sql
    createing 05_cleanup.sql

---
+++ 05_cleanup.sql
@@ -0,0 +1,14 @@
+-- Drop the S3 stage
+DROP STAGE my_s3_stage;
+
+-- Drop the storage integration
+DROP STORAGE INTEGRATION S3_int;
+
+-- Drop the file format
+DROP FILE FORMAT csv_format;
+
+-- Drop the first table
+DROP TABLE try002;
+
+-- Drop the second table
+DROP TABLE upload001;
